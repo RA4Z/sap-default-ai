fc7aea1e4b77ecf93aa0ee77b8908a6c21ba6755
RA4Z <rraz639@gmail.com>
2024-06-12 14:47:33 -0300

    Modelo default desenvolvido para uso de IA

---
+++ main.py
@@ -0,0 +1,17 @@
+from sap_functions import SAP
+
+default_language = 'PT'
+login = open('sap_login.txt', 'r').readline().strip().split(',')
+scheduled_execution = {'scheduled?': False, 'username': login[0], 'password': login[1], 'principal': '100'}
+sap_window = 0
+
+# Python Default Script
+# Default model for SAP automations, developed by Robert Aron Zimmermann, using Google AI Studio tuned prompt model
+
+class Work:
+    def __init__(self):
+        self.sap = SAP(sap_window, scheduled_execution, default_language)
+
+
+if __name__ == '__main__':
+    work = Work()
